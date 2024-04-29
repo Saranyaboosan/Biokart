8ec71158a8f06e0fc5c5c8fda653556bedb31c4d
gurupreethsingh <igurupreeth@gmail.com>
2024-04-29 13:11:44 +0530

    some updates

--- biokart_website_automation/src/opening_all_browsers/OpenChrome.java
+++ biokart_website_automation/src/opening_all_browsers/OpenChrome.java
@@ -22,36 +22,46 @@ public class OpenChrome
 {
   public static void main(String[] args) throws InterruptedException, IOException {
 	  // set the system properties to open chrome browser
-	System.setProperty("webdriver.chrome.driver", "D:\\ecoders_testing_projects\\biokart_testing_project\\Biokart\\biokart_website_automation\\driver_executables\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", 
+			"D:\\ecoders_production_applications\\biokart\\testing\\Biokart\\biokart_website_automation\\driver_executables\\chromedriver.exe");
 	
     // open the web browser by creating an object of Chromedriver class. 
 	WebDriver driver =  new ChromeDriver(); 
 	
 	// implicitly wait for the browser to load completly. 
-//	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	
 	// updated code for implicitlyWait using Duration class,
-	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	
 	// navigate to the biokart website using get() , or navigate().to() method. of WebDriver interface. 
 	
 	driver.get("https://www.biokart.com");
 	
 	// implicitly wait for the browser to load completly. 
-//	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	
+	String actualtitle = driver.getTitle();
+    System.out.println(actualtitle);
 	
 //	String expectedTitle = "Biokart India Pvt Ltd &#8211; Bringing Biotech Closer!";
-	String expectedTitle = "Biokart India Pvt Ltd – Bringing Biotech Closer!";
-//	Expected condition failed: waiting for title to be "Biokart India Pvt Ltd &#8211; Bringing Biotech Closer!". Current title: "Biokart India Pvt Ltd – Bringing Biotech Closer!
+	String expectedTitle = "Biokart India Pvt Ltd â€“ Bringing Biotech Closer!";
+//	Expected condition failed: waiting for title to be "Biokart India Pvt Ltd &#8211; Bringing Biotech Closer!". Current title: "Biokart India Pvt Ltd ï¿½ Bringing Biotech Closer!
 	
 	// check if the title of the biokart.com is matching or not. 
 
 	// check if the title of the biokart.com is matching or not. 
-	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	WebDriverWait wait = new WebDriverWait(driver,10);
 	try {
 	    Boolean element = wait.until(ExpectedConditions.titleIs(expectedTitle));
 	    if (element) {
 	        System.out.println("Test case passed, title is matching.");
+	    	// giving 2 seconds to wait till we close the browser compeletly.
+	    	Thread.sleep(2000);
+	    	
+	    	// close the browser. 
+	    	driver.quit();
 	    }
 	} catch (Exception ex) {
 		System.out.println("Test case failed, Title did not match.");
@@ -63,19 +73,20 @@ public class OpenChrome
 	    // make a location on the hard disk to store the screenshot. 
 	    Date d = new Date();
 	    // Replace special characters in the file name.
-	    String sanitizedTitle = expectedTitle.replace("&", "").replace("–", "-");
-	    File hd = new File("D:\\ecoders_testing_projects\\biokart_testing_project\\Biokart\\biokart_website_automation\\failed_screenshots\\" + sanitizedTitle + "_" + d.getTime() + ".jpg");
+	    String sanitizedTitle = expectedTitle.replace("&", "").replace("ï¿½", "-");
+	    File hd = new File("D:\\ecoders_production_applications\\biokart\\testing\\Biokart\\biokart_website_automation\\failed_screenshots\\" + sanitizedTitle + "_" + d.getTime() + ".jpg");
 	    // copy the file from RAM to the hard disk location. 
 	    Files.copy(ram, hd);
+		// giving 2 seconds to wait till we close the browser compeletly.
+		Thread.sleep(2000);
+		
+		// close the browser. 
+		driver.quit();
 	}
 
 	//similarly we can check if the url of the biokart page is matching or not. 
 	
 	
-	// giving 2 seconds to wait till we close the browser compeletly.
-	Thread.sleep(2000);
-	
-	// close the browser. 
-	driver.quit();
+
   }
 }
